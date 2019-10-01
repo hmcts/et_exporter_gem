f041c3350f909fdb0f849613c2624418dcc8acf2
Gary Taylor <gary.taylor@hismessages.com>
2019-10-01 17:15:08 +0100

    RST-2197 - When claim is queued, the application event is fired to provide feedback

--- app/event_handlers/et_exporter/claim_queued_handler.rb
+++ app/event_handlers/et_exporter/claim_queued_handler.rb
@@ -1,9 +1,20 @@
 require 'sidekiq'
 module EtExporter
   class ClaimQueuedHandler
-    def handle(export)
+    def handle(export, event_service: Rails.application.event_service)
       json = EtExporter::ApplicationController.render('et_exporter/v1/export_claim/export.json.jbuilder', locals: {claim: export.resource, export: export, system: export.external_system})
-      client_push('class' => '::EtExporter::ExportClaimWorker', 'args' => [json], 'queue' => export.external_system.export_queue)
+      jid = client_push('class' => '::EtExporter::ExportClaimWorker', 'args' => [json], 'queue' => export.external_system.export_queue)
+      event_data = {
+        sidekiq: {
+          jid: jid,
+          bid: nil,
+        },
+        export_id: export.id,
+        external_data: {},
+        state: 'queued',
+        percent_complete: nil
+      }
+      event_service.publish('ClaimExportFeedbackReceived', event_data.to_json)
     end
     
     def client_push(item)
