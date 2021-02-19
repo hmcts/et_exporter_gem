b808545aedd69199625313a3dd4cfd8eb1fe30ba
Gary Taylor <gary.taylor@hismessages.com>
2021-02-19 08:53:54 +0000

    Fixed deprecation errors

--- app/event_handlers/et_exporter/claim_queued_handler.rb
+++ app/event_handlers/et_exporter/claim_queued_handler.rb
@@ -2,7 +2,7 @@ require 'sidekiq'
 module EtExporter
   class ClaimQueuedHandler
     def handle(export, event_service: Rails.application.event_service)
-      json = EtExporter::ApplicationController.render('et_exporter/v1/export_claim/export.json.jbuilder', locals: {claim: export.resource, export: export, system: export.external_system})
+      json = EtExporter::ApplicationController.render('et_exporter/v1/export_claim/export', locals: {claim: export.resource, export: export, system: export.external_system}, formats: [:json])
       jid = client_push('class' => '::EtExporter::ExportClaimWorker', 'args' => [json], 'queue' => export.external_system.export_queue)
       event_data = {
         sidekiq: {
