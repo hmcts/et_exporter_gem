a17243e61c49674e37c52986b2efb0b5e9d3ea86
Gary Taylor <gary.taylor@hismessages.com>
2021-05-17 10:22:11 +0100

    Bug fix - external data not being sent in update

--- config/initializers/event_handlers.rb
+++ config/initializers/event_handlers.rb
@@ -1,8 +1,8 @@
 Rails.application.config.after_initialize do |app|
   app.event_service.subscribe('ClaimQueuedForExport', EtExporter::ClaimQueuedHandler, async: false, in_process: true)
-  app.event_service.subscribe('ExportedClaimFilesAdded', EtExporter::ClaimFilesAddedHandler, async: false, in_process: true)
   app.event_service.subscribe('ResponseQueuedForExport', EtExporter::ResponseQueuedHandler, async: false, in_process: true)
   app.event_service.subscribe('ClaimExportFeedbackReceived', EtExporter::ClaimExportFeedbackReceivedHandler, async: true, in_process: false)
   app.event_service.subscribe('ResponseExportFeedbackReceived', EtExporter::ClaimExportFeedbackReceivedHandler, async: true, in_process: false)
+  app.event_service.subscribe('ExportedClaimFilesAdded', EtExporter::ClaimFilesAddedHandler, async: false, in_process: true)
 
 end
